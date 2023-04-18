5728fe2b62e4964cab6442a8dbf726d26551011d
fahmipraga <itmamul2004@gmail.com>
2023-04-18 23:51:41 +0700

    add entrypoint file

---
+++ index.js
@@ -0,0 +1,15 @@
+require("dotenv").config({
+    path: ".env"
+})
+
+const express = require("express")
+const app = express()
+app.use(express.urlencoded({extended: false}))
+const PORT = process.env.PORT
+
+app.use("/", require("./src/routers/index"))
+
+app.listen(PORT, () => {
+    console.log(`App is listening on ${PORT}`)
+})
+ 
