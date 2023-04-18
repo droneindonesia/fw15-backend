dc1b1c7e980f32f71dd3dc2945db7ed6049691aa
fahmipraga <itmamul2004@gmail.com>
2023-04-18 23:57:46 +0700

    add router

---
+++ src/routers/profile.router.js
@@ -0,0 +1,11 @@
+const profileRouter = require("express").Router()
+/* const validate = require("../../middlewares/validator.middleware") */
+const uploadMiddleware = require("../middlewares/upload.middleware")
+const authMiddleware = require("../middlewares/auth.middleware")
+const profileController = require("../controllers/profile.controller")
+
+profileRouter.get("/", profileController.getProfile)
+profileRouter.post("/profile", authMiddleware, uploadMiddleware("picture"), profileController.updateProfile)
+
+module.exports = profileRouter
+ 
