98fcc6e2fde2640e62bb3d04217877f95bc9caf6
fahmipraga <itmamul2004@gmail.com>
2023-04-26 12:16:13 +0700

    fix: add validator for profile for main business flow

--- src/routers/profile.router.js
+++ src/routers/profile.router.js
@@ -1,9 +1,10 @@
 const profileRouter = require("express").Router()
 const uploadMiddleware = require("../middlewares/upload.middleware")
 const profileController = require("../controllers/profile.controller")
+const validate = require("../middlewares/validator.middleware")
 
 profileRouter.get("/", profileController.getProfile)
-profileRouter.post("/", uploadMiddleware("picture"), profileController.updateProfile)
+profileRouter.post("/", uploadMiddleware("picture"), validate("profile"), profileController.updateProfile)
 
 module.exports = profileRouter
  
