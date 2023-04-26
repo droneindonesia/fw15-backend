f8d8c1c46308800ad5db8433eef0ac658c06f550
fahmipraga <itmamul2004@gmail.com>
2023-04-26 15:52:35 +0700

    fix: fix some code in wishlist controller

--- src/controllers/wishlist.controller.js
+++ src/controllers/wishlist.controller.js
@@ -1,12 +1,31 @@
 const errorHandler = require("../helpers/errorHandler.helper")
 const wishlistModel = require("../models/admin/wishlist.model")
 
-exports.getAllModel = async (req, res) => {
+exports.getWishlist = async (req, res) => {
     try {
         let data = await wishlistModel.findOne(req.params.id)
         return res.json({
             success: true,
             message: "Get all wishlist successfully",
+            results: data
+        })
+    } catch (err) {
+        return errorHandler(res, err)
+    }
+}
+
+exports.getAllWishlist = async (req, res) => {
+    try {
+        let data = await wishlistModel.findAll(
+            req.query.page,
+            req.query.limit,
+            req.query.search,
+            req.query.sort,
+            req.query.sortBy
+        )
+        return res.json({
+            success: true,
+            message: "Get All Wishlist successfully",
             results: data
         })
     } catch (err) {
--- src/routers/wishlist.router.js
+++ src/routers/wishlist.router.js
@@ -1,6 +1,7 @@
 const wishlistRouter = require("express").Router()
 const wishlistController = require("../controllers/wishlist.controller")
 
-wishlistRouter.get("/", wishlistController.getWishlist)
+wishlistRouter.get("/:id", wishlistController.getWishlist)
+wishlistRouter.get("/", wishlistController.getAllWishlist)
 
 module.exports = wishlistRouter
