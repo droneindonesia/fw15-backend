72fbb3fa66ce11be63dea7ac84db25c3aaf7f87c
fahmipraga <itmamul2004@gmail.com>
2023-05-02 01:32:46 +0700

    fix: add uppload middleware in events

--- src/routers/city.router.js
+++ src/routers/city.router.js
@@ -2,6 +2,6 @@ const cityRouter = require("express").Router()
 const citiesController = require("../controllers/cities.controller")
 const validate = require("../middlewares/validator.middleware")
 
-cityRouter.get("/",validate("getAll"), citiesController.getAll)
+cityRouter.get("/", validate("getAll"), citiesController.getAll)
 
 module.exports = cityRouter
--- src/routers/events.router.js
+++ src/routers/events.router.js
@@ -1,10 +1,11 @@
 const eventsRouter = require("express").Router()
 const eventsController = require("../controllers/events.controller")
 const validate = require("../middlewares/validator.middleware")
+const uploadMiddleware = require("../middlewares/upload.middleware")
 
 eventsRouter.get("/", validate("getAll"), eventsController.getAllEvents)
 eventsRouter.get("/", validate("getOne"), eventsController.getEventsById)
-eventsRouter.post("/manage", validate("events"), eventsController.addEvents)
-eventsRouter.patch("/manage", validate("events"), eventsController.updateEvents)
+eventsRouter.post("/manage", uploadMiddleware("picture"), validate("events"), eventsController.addEvents)
+eventsRouter.patch("/manage", uploadMiddleware("picture"), validate("events"), eventsController.updateEvents)
 
 module.exports = eventsRouter
