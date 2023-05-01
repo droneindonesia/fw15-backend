de4fc1f6e35a6b48b5d45a5510981868fab1da50
fahmipraga <itmamul2004@gmail.com>
2023-05-02 01:51:58 +0700

    fix: add path variables to wishlist router

--- src/routers/wishlist.router.js
+++ src/routers/wishlist.router.js
@@ -2,7 +2,7 @@ const wishlistRouter = require("express").Router()
 const wishlistController = require("../controllers/wishlist.controller")
 const validate = require("../middlewares/validator.middleware")
 
-wishlistRouter.get("/", validate("getOne"), wishlistController.getWishlist)
+wishlistRouter.get("/:id", validate("getOne"), wishlistController.getWishlist)
 wishlistRouter.get("/", validate("getAll"), wishlistController.getAllWishlist)
 
 module.exports = wishlistRouter
