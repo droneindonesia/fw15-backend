8279e51ad6ffdd84edb84e2ec314841fd56f5e39
fahmipraga <itmamul2004@gmail.com>
2023-05-02 15:31:53 +0700

    fix: fix some code in controller

--- src/controllers/payment.controller.js
+++ src/controllers/payment.controller.js
@@ -1,13 +1,11 @@
 const errorHandler = require("../helpers/errorHandler.helper")
-const paymentsModel = require("../models/admin/reservations.model")
+// const paymentsModel = require("../models/admin/reservations.model")
+// const reservationTicketModel = require("../models/admin/reservationsticket.model")
 
 exports.createPayments = async (req, res) => {
     try {
-        let data = await paymentsModel.insert(req.body)
         return res.json({
-            success: true,
-            message: "Create payments successfully",
-            results: data
+            success: true
         })
     } catch (err) {
         return errorHandler(res, err)
--- src/controllers/reservations.controller.js
+++ src/controllers/reservations.controller.js
@@ -1,14 +1,19 @@
-const reservationsTicketModel = require("../models/admin/reservationsticket.model")
+const reservationsModel = require("../models/admin/reservations.model")
 const errorHandler = require("../helpers/errorHandler.helper")
 
 exports.createReservations = async (req, res) => {
     try {
-        let data = await reservationsTicketModel.insert(req.body)
+        const { id } = req.user
+
+        const data = {userId: id, ...req.body}
+
+        const reservation = await reservationsModel.insert(data)
         return res.json({
             success: true,
-            message: "Create reservation successfully",
-            results: data
+            message: "Add reservations successfully",
+            results: reservation
         })
+
     } catch (err) {
         return errorHandler(res, err)
     }
