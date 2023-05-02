9f8f74d63df3b295d1479f01a1bf5ccd19784f6a
fahmipraga <itmamul2004@gmail.com>
2023-05-02 10:40:58 +0700

    fix: add path variables

--- src/routers/events.router.js
+++ src/routers/events.router.js
@@ -4,7 +4,7 @@ const validate = require("../middlewares/validator.middleware")
 const uploadMiddleware = require("../middlewares/upload.middleware")
 
 eventsRouter.get("/", validate("getAll"), eventsController.getAllEvents)
-eventsRouter.get("/", validate("getOne"), eventsController.getEventsById)
+eventsRouter.get("/:id", validate("getOne"), eventsController.getEventsById)
 eventsRouter.post("/manage", uploadMiddleware("picture"), validate("events"), eventsController.addEvents)
 eventsRouter.patch("/manage", uploadMiddleware("picture"), validate("events"), eventsController.updateEvents)
 
