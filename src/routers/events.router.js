6f5e6a7abaa23d2f99b20be464faa6a49029a995
fahmipraga <itmamul2004@gmail.com>
2023-05-05 09:28:32 +0700

    fix: fix some code in events router, add postman file

--- src/routers/events.router.js
+++ src/routers/events.router.js
@@ -5,7 +5,10 @@ const uploadMiddleware = require("../middlewares/upload.middleware")
 
 eventsRouter.get("/", validate("getAll"), eventsController.getAllEvents)
 eventsRouter.get("/:id", validate("getOne"), eventsController.getEventsById)
-eventsRouter.post("/manage", uploadMiddleware("picture"), validate("events"), eventsController.addEvents)
+eventsRouter.get("/manage", eventsController.getEvents)
+eventsRouter.post("/manage/:id", uploadMiddleware("picture"), validate("events"), eventsController.addEvents)
 eventsRouter.patch("/manage", uploadMiddleware("picture"), validate("events"), eventsController.updateEvents)
+eventsRouter.get("/manage/:id", eventsController.destroy)
+
 
 module.exports = eventsRouter
