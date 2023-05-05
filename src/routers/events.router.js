1f2ee9028c366d491d3da75becb34d1f84d52265
fahmipraga <itmamul2004@gmail.com>
2023-05-05 09:43:57 +0700

    fix: fix code in events router

--- src/routers/events.router.js
+++ src/routers/events.router.js
@@ -6,7 +6,7 @@ const uploadMiddleware = require("../middlewares/upload.middleware")
 eventsRouter.get("/", validate("getAll"), eventsController.getAllEvents)
 eventsRouter.get("/:id", validate("getOne"), eventsController.getEventsById)
 eventsRouter.get("/manage", eventsController.getEvents)
-eventsRouter.post("/manage/:id", uploadMiddleware("picture"), validate("events"), eventsController.addEvents)
+eventsRouter.post("/manage/:id", uploadMiddleware("picture"), validate("events"), eventsController.createEvent)
 eventsRouter.patch("/manage", uploadMiddleware("picture"), validate("events"), eventsController.updateEvents)
 eventsRouter.get("/manage/:id", eventsController.destroy)
 
