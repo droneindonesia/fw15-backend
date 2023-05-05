893bcb5591cb055f6ae3194eb754ca46c5264ff6
fahmipraga <itmamul2004@gmail.com>
2023-05-05 09:07:29 +0700

    fix: add reservations ticket successfully

--- src/routers/reservations.router.js
+++ src/routers/reservations.router.js
@@ -3,5 +3,6 @@ const reservationsController = require("../controllers/reservations.controller")
 const validate = require("../middlewares/validator.middleware")
 
 reservationsRouter.post("/", validate("reservationsticket"), reservationsController.createReservations)
+reservationsRouter.post("/ticket", reservationsController.makeTicket)
 
 module.exports = reservationsRouter
