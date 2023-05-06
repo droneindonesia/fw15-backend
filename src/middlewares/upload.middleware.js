1610b9be078f3618436edbfd3b8a1e1324898866
fahmipraga <itmamul2004@gmail.com>
2023-05-06 17:54:48 +0700

    fix: fix cloudinary

--- src/middlewares/upload.middleware.js
+++ src/middlewares/upload.middleware.js
@@ -2,6 +2,7 @@ require("dotenv").config()
 const multer = require("multer")
 const cloudinary = require("cloudinary").v2
 const { CloudinaryStorage } = require("multer-storage-cloudinary")
+const path = require("path")
 
 cloudinary.config({
     cloud_name: process.env.CLOUD_NAME,
