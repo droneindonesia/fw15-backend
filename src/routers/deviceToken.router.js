3cff483e2b84211761db0e689f3108d00d63e687
fahmipraga <itmamul2004@gmail.com>
2023-07-04 22:40:50 +0700

    fix: router

--- src/routers/deviceToken.router.js
+++ src/routers/deviceToken.router.js
@@ -1,6 +1,6 @@
 const deviceTokenRouter = require("express").Router()
 const deviceTokenController = require("../controllers/deviceToken.controller")
 
-deviceTokenRouter.post("/device-token", deviceTokenController.saveToken)
+deviceTokenRouter.post("/", deviceTokenController.saveToken)
 
 module.exports = deviceTokenRouter
