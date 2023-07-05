fc0e4d34c1ea7dfe145ada171399bd686441b985
fahmipraga <itmamul2004@gmail.com>
2023-07-05 22:19:42 +0700

    fix: device token model

--- src/models/admin/deviceToken.model.js
+++ src/models/admin/deviceToken.model.js
@@ -2,19 +2,20 @@ const db = require("../../helpers/db.helper")
 
 const table = "devicetoken"
 
-exports.findAll = async function (page, limit, search, sort, sortBy) {
+exports.findAll = async function (page, limit, sort, sortBy) {
     page = parseInt(page) || 1
     limit = parseInt(limit) || 5
-    search = search || ""
     sort = sort || "id"
     sortBy = sortBy || "ASC"
 
     const offset = (page - 1) * limit
     const query = `
-  SELECT * FROM ${table} WHERE "name" LIKE $3 ORDER BY ${sort} ${sortBy} LIMIT $1 OFFSET $2
-  `
+    SELECT * FROM ${table} 
+    ORDER BY ${sort} ${sortBy} 
+    LIMIT $1 OFFSET $2
+    `
 
-    const values = [limit, offset, `%${search}%`]
+    const values = [limit, offset]
     const { rows } = await db.query(query, values)
 
     return rows
