bcbe50314258a26445a9a6887719152d706ac6af
fahmipraga <itmamul2004@gmail.com>
2023-07-06 10:13:52 +0700

    fix: firebase

--- src/helpers/firebase.js
+++ src/helpers/firebase.js
@@ -1,6 +1,6 @@
 var admin = require("firebase-admin")
 
-var serviceAccount = require("../../eventpraga-f4b4d-firebase-adminsdk-p87vd-37c2c01bb2.json")
+var serviceAccount = require("../../firebase")
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
